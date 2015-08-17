46a9db9f10b08f8057d40d04d7415ae4ac0ede5d
Sai Pc <schand31@asu.edu>
2015-08-16 21:23:21 -0700

    connected the fragments
    added transitions

--- workouts/src/main/java/com/tda/saipc/workouts/workouts/MainActivity.java
+++ workouts/src/main/java/com/tda/saipc/workouts/workouts/MainActivity.java
@@ -1,19 +1,27 @@
 package com.tda.saipc.workouts.workouts;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements WorkoutListFragment.WorkoutListListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WorkoutDetailFragment workoutDetailFragment = (WorkoutDetailFragment) getFragmentManager().findFragmentById(R.id.detailFragment);
-        workoutDetailFragment.setWorkoutID(1);
     }
 
+    @Override
+    public void itemClicked(int position) {
+        WorkoutDetailFragment workoutDetailFragment = new WorkoutDetailFragment();
+        workoutDetailFragment.setWorkoutID(position);
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainer, workoutDetailFragment);
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.commit();
+    }
 }
--- workouts/src/main/java/com/tda/saipc/workouts/workouts/WorkoutListFragment.java
+++ workouts/src/main/java/com/tda/saipc/workouts/workouts/WorkoutListFragment.java
@@ -1,6 +1,7 @@
 package com.tda.saipc.workouts.workouts;
 
 
+import android.app.Activity;
 import android.app.ListFragment;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -22,14 +23,37 @@ public class WorkoutListFragment extends ListFragment {
         // Required empty public constructor
     }
 
+    // define a static interface, which can be implemented by the activities
+    // that want to listen to list-clicks
+    static interface WorkoutListListener {
+        void itemClicked(int position);
+    }
 
+    private WorkoutListListener listener;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ArrayAdapter<Workout> listAdapter = new ArrayAdapter<Workout>(inflater.getContext(), android.R.layout.simple_list_item_1, Workout.workouts);
+        // works cause toString overridden in workouts --> good going Pc.
+        ArrayAdapter<Workout> listAdapter = new ArrayAdapter<Workout>(
+                inflater.getContext(),
+                android.R.layout.simple_list_item_1,
+                Workout.workouts
+            );
         setListAdapter(listAdapter);
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        this.listener = (WorkoutListListener) activity;
+    }
 
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        if (this.listener != null) {
+            this.listener.itemClicked(position);
+        }
+    }
 }
